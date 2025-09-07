e8ee4ba36394f418800008dbcbac55e4bc7fe8f9
Matt Young <halcyondude@gmail.com>
2025-09-07 01:15:32 -0400

    feat: make simple blogs graphql endpoint w/ federattion primitives

--- index.js
+++ index.js
@@ -1,48 +1,87 @@
-import { createServer } from 'node:http'
-import { createYoga } from 'graphql-yoga'
-import { createSchema } from 'graphql-yoga'
+import { createServer } from 'node:http';
+import { createYoga } from 'graphql-yoga';
 
-const typeDefs = /* GraphQL */ `
-  type Query {
-    blogs: [Blog!]
-    blog(id: ID!): Blog
-  }
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import gql from 'graphql-tag';
+import fs from 'fs';
+import path from 'path';
 
-  type Blog @key(fields: "id") {
-    id: ID!
-    title: String!
-    author: Author
-  }
+// Load schema from file
+const typeDefs = gql(fs.readFileSync(path.join(process.cwd(), 'schema.graphql'), 'utf8'));
 
-  # We are extending the Author type, which is owned by the authors subgraph.
-  # The @key directive tells the gateway how to fetch an Author.
-  # The @extends directive tells the gateway this type is defined elsewhere.
-  type Author @key(fields: "id") @extends {
-    id: ID! @external
-  }
-`;
+// Load authors and blogs as separate arrays from data.json
+const { authors, blogs } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'data.json'), 'utf8'));
 
-const blogsData = [
-  { id: '1', title: 'Introduction to GraphQL', authorId: '101' },
-  { id: '2', title: 'Federation Deep Dive', authorId: '102' },
-];
-
+// GraphQL resolvers map schema fields to data and logic
+//
+// Understanding the pattern: authors.find(a => a.id === id)
+//
+// - The => is the arrow function syntax in JavaScript/TypeScript. It defines an anonymous function.
+//   Example: a => a.id === id is equivalent to function(a) { return a.id === id; }
+//
+// - The === is the strict equality operator. It checks that both value and type are equal (no type coercion).
+//   Example: '2' === 2 is false, but 2 === 2 is true.
+//
+// - .find() is a built-in array method that takes a function (predicate) and returns the first element for which the function returns true.
+//
+// - So, authors.find(a => a.id === id) searches the authors array for an author object whose id property matches the id argument exactly.
+//   If a match is found, that author object is returned; if not, .find() returns undefined.
+//
+// Example:
+//   authors = [ { id: '1', name: 'Ada' }, { id: '2', name: 'Grace' } ]
+//   id = '2'
+//   authors.find(a => a.id === id) // returns { id: '2', name: 'Grace' }
+//
+// This pattern is used for both authors and blogs to efficiently look up a single item by its unique id.
 const resolvers = {
   Query: {
-    blogs: () => blogsData,
-    blog: (_, { id }) => blogsData.find(b => b.id === id),
+    // Returns all blogs in the system as an array.
+    blogs: () => blogs,
+
+    // Returns a single blog by its unique id.
+    blog: (_, { id }) => blogs.find(b => b.id === id),
+
+    // Returns all authors in the system as an array.
+    authors: () => authors,
+
+    // Returns a single author by their unique id.
+    author: (_, { id }) => authors.find(a => a.id === id),
   },
   Blog: {
-    // This resolver provides the "link" to the Author type.
-    // It returns a representation containing the key field ('id').
-    // The gateway will use this to fetch the full Author from the other subgraph.
-    author: (blog) => {
-      return { __typename: "Author", id: blog.authorId };
-    }
+    // Return the author for a blog
+    author: (blog) => authors.find(a => a.id === blog.authorId),
+    // Return the labels for a blog
+    labels: (blog) => blog.labels
+  },
+  Author: {
+    // Return all blogs for an author
+    blogs: (author) => blogs.filter(b => b.authorId === author.id)
+    // name, age, description are resolved automatically
   }
 };
 
-const yoga = createYoga({ schema: createSchema({ typeDefs, resolvers }) });
+// Build the schema with federation support
+const schema = buildSubgraphSchema([{ typeDefs, resolvers }]);
+
+// Add logging plugin for Yoga
+const yoga = createYoga({
+  schema,
+  plugins: [
+    {
+      onExecute({ args }) {
+        const { document, variableValues, operationName } = args;
+        console.info('--- Incoming GraphQL Request ---');
+        console.info('Operation:', operationName);
+        console.info('Query:', document && document.loc && document.loc.source.body);
+        console.info('Variables:', variableValues);
+        console.info('-------------------------------');
+      }
+    }
+  ]
+});
+
 const server = createServer(yoga);
 const PORT = process.env.PORT || 4001;
-server.listen(PORT, () => { console.info(`Blogs subgraph running at http://localhost:${PORT}/graphql`) });
+server.listen(PORT, () => {
+  console.info(`Blogs subgraph running at http://localhost:${PORT}/graphql`)
+});
